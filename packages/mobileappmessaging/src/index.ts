7ecb5016862561a58260412604dce4c1b146d3b9
Sunny Agarwal <sunny.agarwal@salesforce.com>
2026-05-26 15:56:39 +0530

    Fixed all the packages and podspec details and added precommit hooks

--- packages/mobileappmessaging/src/index.ts
+++ packages/mobileappmessaging/src/index.ts
@@ -1,7 +1,40 @@
-// Product packages MUST NOT re-export sibling packages. Customers import shared
-// modules (PushModule, IamModule, SFMCSdkModule) directly from their own packages.
-// We do export each name twice (canonical + short alias matching iOS RN bridge class)
-// so example apps that use either form resolve to the same value.
-export { MobileAppMessagingModule, MobileAppMessagingModule as MAMModule } from './MobileAppMessagingModule';
-export type { MobileAppMessagingApi, MobileAppMessagingApi as MAMApi } from './types';
-export type { Registration } from './types';
+/**
+ * @license
+ * Copyright 2026 Salesforce, Inc
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+export {
+  MobileAppMessagingModule,
+  MobileAppMessagingModule as MAMModule,
+} from "./MobileAppMessagingModule";
+export type {
+  MobileAppMessagingApi,
+  MobileAppMessagingApi as MAMApi,
+} from "./types";
+export type { Registration } from "./types";
